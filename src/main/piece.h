28567f95ac87a30c7dcf1beac92adc72b752ad8d
michaelallan <mb.allan@ymail.com>
2022-03-22 11:57:49 -0700

    Initial commit

---
+++ src/main/piece.h
@@ -0,0 +1,14 @@
+#ifndef __PIECE_H__
+#define __PIECE_H__
+
+namespace Chess {
+    /**
+     * @brief A chess piece that can be moved on a chess board
+     * 
+     */
+    class Piece {
+        
+    };
+}
+
+#endif // __PIECE_H__
